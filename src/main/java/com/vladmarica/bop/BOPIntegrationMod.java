b9b20a31bd8a8f8aaba00e80b150e54abc935f88
Quintinity <vlad.marica1@gmail.com>
2018-04-05 01:04:23 -0300

    Added biome block to end

--- src/main/java/com/vladmarica/bop/BOPIntegrationMod.java
+++ src/main/java/com/vladmarica/bop/BOPIntegrationMod.java
@@ -1,9 +1,11 @@
 package com.vladmarica.bop;
 
 import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.registry.GameRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,6 +14,11 @@ public class BOPIntegrationMod {
 
     static final String MODID = "BOPIntegration";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        GameRegistry.registerWorldGenerator(new BOPLegacyWorldGenerator(), 0);
+    }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
